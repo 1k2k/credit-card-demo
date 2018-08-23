54a4d75d8e627def307d25b56165f86c81d94725
1k2k <14908307+1k2k@users.noreply.github.com>
2018-08-22 23:41:45 -0400

    assetPublicPath f

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/1k2k/credit-card-demo',
+    assetsPublicPath: '/credit-card-demo',
 
     /**
      * Source Maps
